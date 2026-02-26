e2256f5fceec015cc91c3dcd857b9ea69844a919
shuiyued <875155591@qq.com>
2026-02-26 20:09:19 +0800

    GeoMotion

--- train.sh
+++ train.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # set -euo pipefail
 
-CUDA_DEVICE=${CUDA_DEVICE:-0}
-MASTER_PORT=${MASTER_PORT:-29502}
-NPROC_PER_NODE=${NPROC_PER_NODE:-1}
-CONFIG_PATH=${CONFIG_PATH:-configs/pi3_conf_low_35_feature_flow_gotm_verse_stop_all.yaml}
-
-CUDA_VISIBLE_DEVICES="$CUDA_DEVICE" torchrun \
-  --master-port="$MASTER_PORT" \
-  --nproc_per_node="$NPROC_PER_NODE" \
-  train.py "$CONFIG_PATH"
+CUDA_VISIBLE_DEVICES=3,4,6,7 torchrun --master-port=29501 --nproc_per_node=4 train.py configs/pi3_conf_low_35_feature_flow_gotm_verse_stop_all.yaml
